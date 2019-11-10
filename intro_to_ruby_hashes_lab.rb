a890c23de80e702360404f4da5b5c9ae6246247e
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-10 02:30:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,7 +49,7 @@ monopoly = {
   :railroads => {
    :pieces => 4, 
    :names => {
-     :reading_railroad => {""},
+     :reading_railroad => {"mortgage_value" => "$100", },
      :pennsylvania_railroad => {},
      :b_and_o_railroad => {}, 
      :shortline => {}
